dab982f7af88d84adc7270f8f7f396b09b09afaa
Shankar Rao <shankar1093@gmail.com>
2024-07-14 15:06:37 +0530

    added a health endpoint to the frontend

---
+++ frontend/src/pages/api/health.tsx
@@ -0,0 +1,8 @@
+// src/pages/api/health.tsx
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({
+    status: 'ok',
+    });
+}
