14fd193e48f3ac844f22b74a9fd4d07db6c553e6
Shankar Rao <shankar1093@gmail.com>
2024-07-19 00:29:38 +0530

    updated to round up or down by rs 50

--- frontend/src/pages/api/gold_price.tsx
+++ frontend/src/pages/api/gold_price.tsx
@@ -1,6 +1,18 @@
 // src/pages/api/gold_price.js
 import { NextApiRequest, NextApiResponse } from 'next';
 
+function price_adjustment(price: number) {
+  let rounding_val = price % 100;
+  if (rounding_val > 25) {
+    price = price - rounding_val + 100;
+  } else if (rounding_val > 65) { // Adjusted condition to correctly check the value
+    price = price - rounding_val + 50;
+  } else {
+    price = price - rounding_val;
+  }
+  return price;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   let gold995WithGST = null;
   try {
@@ -14,12 +26,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(500).json({ error: 'Error fetching gold price data' });
   }
 
-  const gold24ktPrice = gold995WithGST && !isNaN(parseFloat(gold995WithGST.bid)) ? parseFloat(gold995WithGST.bid) / 10 : 7200;
-  const gold22ktPrice = !isNaN(gold24ktPrice) ? (916 / 995) * gold24ktPrice : 6500;
+  const gold24ktPrice = gold995WithGST && !isNaN(parseFloat(gold995WithGST.bid)) ? parseFloat(gold995WithGST.bid) / 10 : 0;
+  const gold22ktPrice = !isNaN(gold24ktPrice) ? (916 / 995) * gold24ktPrice : 0;
+
+  const adjustedGold22ktPrice = price_adjustment(gold22ktPrice);
+  const adjustedGold24ktPrice = price_adjustment(gold24ktPrice);
+  
 
   res.status(200).json({
-    gold22kt: gold22ktPrice,
-    gold24kt: gold24ktPrice,
+    gold22kt: adjustedGold22ktPrice,
+    gold24kt: adjustedGold24ktPrice,
     date: new Date().toLocaleDateString("en-IN"),
   });
 }
