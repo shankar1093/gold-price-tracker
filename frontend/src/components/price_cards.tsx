48c6d3ec5e1de794f6ebae7e1244eb4309406955
Shankar Rao <shankar1093@gmail.com>
2024-08-27 19:06:16 +0530

    updating the css to align items properly

--- frontend/src/components/price_cards.tsx
+++ frontend/src/components/price_cards.tsx
@@ -12,10 +12,10 @@ const PriceCard: React.FC<PriceCardProps> = ({ title, price, date }) => {
   const displayPrice = isNaN(Number(price)) ? 'N/A' : Math.floor(price || 0);
 
   return (
-      <Card className="flex-1 min-w-[300px] m-2 h-[200px]"> 
+      <Card className="flex-1 min-w-[300px] m-2 h-[180px]"> 
         <CardHeader className="text-2xl font-bold">{title}</CardHeader>
         <CardBody>
-          <div className="text-7xl font-bold mb-2">
+          <div className="text-5xl font-bold mb-2">
             ₹<span className="text-primary">{displayPrice}</span>
           </div>
           <p className="text-muted-foreground text-xl">As of {date}</p>
