7c86253460efce39038e46e17e9ab867d07b625a
Shankar Rao <shankar1093@gmail.com>
2024-09-01 01:12:22 +0530

    added a rate model, view to get the api response, settings and url updates

--- backend/mjw_services/gold_rate_admin/models.py
+++ backend/mjw_services/gold_rate_admin/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Rate(models.Model):
-    date = models.DateField()
-    rate_22kt = models.FloatField()
-    rate_24kt = models.FloatField()
-    arihant_rate_22kt = models.FloatField(default=6850)
-    arhiant_rate_24kt = models.FloatField(default=7500)
+    date = models.DateField(unique=True)
+    rate_22kt = models.IntegerField()
+    rate_24kt = models.IntegerField()
+    arihant_rate_22kt = models.IntegerField(default=6850)
+    arihant_rate_24kt = models.IntegerField(default=7500)
 
     def __str__(self):
-        return str(self.date) + " - " + str(self.rate_22kt) + " - " + str(self.rate_24kt) + " - " + str(self.arihant_rate_22kt) + " - " + str(self.arhiant_rate_24kt)
+        return f"{self.date} - 22kt: {self.rate_22kt}, 24kt: {self.rate_24kt}, Arihant 22kt: {self.arihant_rate_22kt}, Arihant 24kt: {self.arihant_rate_24kt}"
+
+    class Meta:
+        ordering = ['-date']
