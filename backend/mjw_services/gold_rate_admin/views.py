48e75af966546b5c47e62cdd206fea1f50a3cd54
Shankar Rao <shankar1093@gmail.com>
2024-09-11 08:54:54 +0530

    update the api to retrieve yesterday's rate till the update happens

--- backend/mjw_services/gold_rate_admin/views.py
+++ backend/mjw_services/gold_rate_admin/views.py
@@ -7,15 +7,21 @@ def index(request):
     return HttpResponse("Hello, world. You're at the mjw_services index.")
 
 def get_gold_rate(request):
-    today = timezone.now().strftime('%Y-%m-%d')
+    today = timezone.now().date()
     try:
+        # First, try to get today's rate
         rate = Rate.objects.get(date=today)
-        return JsonResponse({
-            'date': rate.date,
-            'rate_22kt': rate.rate_22kt,
-            'rate_24kt': rate.rate_24kt,
-            'arihant_rate_22kt': rate.arihant_rate_22kt,
-            'arihant_rate_24kt': rate.arihant_rate_24kt,
-        })
     except Rate.DoesNotExist:
-        return JsonResponse({'error': 'No rate available for today'}, status=404)
+        # If today's rate doesn't exist, get the most recent rate
+        rate = Rate.objects.order_by('-date').first()
+        
+        if not rate:
+            return JsonResponse({'error': 'No rate available'}, status=404)
+
+    return JsonResponse({
+        'date': rate.date,
+        'rate_22kt': rate.rate_22kt,
+        'rate_24kt': rate.rate_24kt,
+        'arihant_rate_22kt': rate.arihant_rate_22kt,
+        'arihant_rate_24kt': rate.arihant_rate_24kt,
+    })
