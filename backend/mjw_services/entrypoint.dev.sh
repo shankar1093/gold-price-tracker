8a730490350fbe49bd59246e05c62c4cfb126eda
Shankar Rao <shankar1093@gmail.com>
2025-05-25 23:45:51 +0530

    Added Local testing Makefile and docker files, added caching to image carousel, updated ig access token

---
+++ backend/mjw_services/entrypoint.dev.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for PostgreSQL..."
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Setting up cron job for gold_rate_update script..."
+echo "30 3 * * * /usr/local/bin/python /app/scripts/gold_rate_update.py >> /var/log/cron.log 2>&1" > /etc/cron.d/gold_rate_update
+chmod 0644 /etc/cron.d/gold_rate_update
+crontab /etc/cron.d/gold_rate_update
+
+# Start cron service
+service cron start
+
+echo "Starting Django server..."
+exec "$@"
--- backend/mjw_services/gold_rate_admin/models.py
+++ backend/mjw_services/gold_rate_admin/models.py
@@ -8,6 +8,8 @@ class Rate(models.Model):
     rate_24kt = models.IntegerField()
     arihant_rate_22kt = models.IntegerField(default=6850)
     arihant_rate_24kt = models.IntegerField(default=7500)
+    arihant_rate_18kt = models.IntegerField(default=7500)
+
 
     def __str__(self):
         return f"{self.date} - 22kt: {self.rate_22kt}, 24kt: {self.rate_24kt}, Arihant 22kt: {self.arihant_rate_22kt}, Arihant 24kt: {self.arihant_rate_24kt}"
