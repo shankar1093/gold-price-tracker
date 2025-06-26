6ea882234077f899327b972eb1a52eb80ab027fd
Shankar Rao <shankar1093@gmail.com>
2025-06-27 00:26:15 +0530

    updated price cards to account for silver and removed date from every individual card

--- frontend/src/components/price_cards.tsx
+++ frontend/src/components/price_cards.tsx
@@ -5,25 +5,19 @@ import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/react';
 interface PriceCardProps {
   title: string;
   price: undefined | number;
-  date: string | undefined;
 }
 
-const PriceCard: React.FC<PriceCardProps> = ({ title, price, date }) => {
+const PriceCard: React.FC<PriceCardProps> = ({ title, price }) => {
   const displayPrice = isNaN(Number(price)) ? 'N/A' : Math.floor(price || 0);
 
   return (
-<Card className="w-full h-auto transform transition-transform hover:scale-105 p-3 md:p-4">
-  <CardHeader className="text-base sm:text-lg md:text-xl lg:text-2xl font-semibold px-2">
-    {title}
+<Card className="w-full h-auto transform transition-transform hover:scale-105 p-4 md:p-[22px] xl:p-8">
+  <CardHeader className="flex justify-between items-center text-base sm:text-lg md:text-xl lg:text-2xl font-semibold px-2">
+    <span>{title}</span>
+    <span className="text-2xl sm:text-3xl md:text-4xl font-bold text-primary">
+      ₹{displayPrice}
+    </span>
   </CardHeader>
-  <CardBody className="px-2">
-    <div className="text-2xl sm:text-3xl md:text-4xl font-bold mb-1">
-      ₹<span className="text-primary">{displayPrice}</span>
-    </div>
-    <p className="text-sm sm:text-base md:text-lg text-muted-foreground">
-      As of {date}
-    </p>
-  </CardBody>
 </Card>
   );
 };
