248fac6cd3f4af6654b7d1788d4a73495c0059ad
Shankar Rao <shankar1093@gmail.com>
2026-02-08 00:12:16 +0530

    updating 24kt gold percentage

--- backend/mjw_services/scripts/gold_rate_update.py
+++ backend/mjw_services/scripts/gold_rate_update.py
@@ -132,8 +132,8 @@ def update_metal_rate():
             gold22ktPrice * 1.011
         )  # Increased by 1.1%
         adjustedGold24ktPrice = price_adjustment(
-            gold24ktPrice * 1.04
-        )  # Increased by 4% starting 1/16 on account of unstability in gold price.
+            gold24ktPrice * 1.03
+        )  # Increased by 3% starting 2/8 on account of unstability in gold price. Will be increasing cash price
 
         metal_prices["rate_18kt"] = math.floor(adjustedGold18ktPrice)
         metal_prices["rate_22kt"] = math.floor(adjustedGold22ktPrice)
