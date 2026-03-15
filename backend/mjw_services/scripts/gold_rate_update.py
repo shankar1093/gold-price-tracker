5461716b7094760626cbb2aba5b1bf85555475ae
Shankar Rao <shankar1093@gmail.com>
2026-03-15 21:27:10 +0530

    updating the 24k margin to 2% to encourage some coin sales

--- backend/mjw_services/scripts/gold_rate_update.py
+++ backend/mjw_services/scripts/gold_rate_update.py
@@ -132,7 +132,7 @@ def update_metal_rate():
             gold22ktPrice * 1.011
         )  # Increased by 1.1%
         adjustedGold24ktPrice = price_adjustment(
-            gold24ktPrice * 1.03
+            gold24ktPrice * 1.02
         )  # Increased by 3% starting 2/8 on account of unstability in gold price. Will be increasing cash price
 
         metal_prices["rate_18kt"] = math.floor(adjustedGold18ktPrice)
